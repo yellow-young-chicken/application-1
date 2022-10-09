f86a168b0706df82b66a57c4a613764a9b2d6335
EC2 Default User <ec2-user@ip-172-31-43-36.ap-northeast-1.compute.internal>
2022-10-09 08:14:34 +0000

    [finish 3-6]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -26,7 +26,7 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
@@ -36,7 +36,7 @@ class ListsController < ApplicationController
     private
     # ストロングパラメータ
     def list_params
-      params.require(:list).permit(:title,:body)
+      params.require(:list).permit(:title,:body,:image)
     end
 
 end
